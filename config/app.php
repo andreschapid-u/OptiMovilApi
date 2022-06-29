794acc65bca07e3e0d33d8f3c01df7631ddcb6ad
Carlos Andres Chapid Inga <carloschapid@unicauca.edu.co>
2022-06-28 23:56:24 -0500

    add url and debug

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Optimovil'),
 
     /*
     |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'env' => env('APP_ENV', 'production'),
+    'env' => env('APP_ENV', 'debug'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://optimovilapi.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
